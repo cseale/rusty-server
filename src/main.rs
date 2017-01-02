b7c6749c5e638eca4bedfbe24b75f3c8cca200eb
Colm Seale <colm.seale@gmail.com>
2017-01-02 01:42:01 +0000

    created stable read/response TCP server

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,30 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 
 // traits
+use std::io::Read;
 use std::io::Write;
 
 fn handle_client(mut stream: TcpStream) {
-    stream.write(b"Hello World\n");
+    let mut buf;
+    loop {
+        // clear out the buffer so we don't send garbage
+        buf = [0; 512];
+        let _ = match stream.read(&mut buf) {
+            Err(e) => panic!("Got an error: {}", e),
+            Ok(m) => {
+                if m == 0 {
+                    // we've got an EOF
+                    break;
+                }
+                m
+            },
+        };
+
+        match stream.write(b"HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\nPong!\r\n") {
+            Err(_) => break,
+            Ok(_) => continue,
+        }
+    }
 }
 
 fn main() {
